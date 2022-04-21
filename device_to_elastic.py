f392a6b7e2769eb5ed477705674cdd9052e91e4a
Panagiotis Stalidis <stalidis@iti.gr>
2022-04-22 02:00:50 +0300

    go to elastic!
    Signed-off-by: Panagiotis Stalidis <stalidis@iti.gr>

---
+++ device_to_elastic.py
@@ -0,0 +1,133 @@
+import asyncio
+import elasticsearch
+from datetime import datetime
+
+elastic_settings = {
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 1
+    },
+    "mappings": {
+        "properties": {
+            "device": {
+                "type": "keyword"
+            },
+            "operation": {
+                "type": "keyword"
+            },
+            "datetime": {
+                "type": "date",
+                "format": "yyyy-MM-dd HH:mm:ss"
+            },
+            "location": {
+                "type": "geo_point"
+            },
+            "F": {
+                "type": "keyword"
+            },
+            "A": {
+                "type": "keyword"
+            },
+            "pitch": {
+                "type": "float"
+            },
+            "heading": {
+                "type": "float"
+            },
+        }
+    }
+}
+
+es = elasticsearch.Elasticsearch(["elastic:9200"])
+if not es.indices.exists(index='cobra'):
+    es.indices.create(index='cobra', body=elastic_settings)
+
+
+def dms2dd(sample, head):
+    # example: s = """2257.702"""
+    degrees, minutes = divmod(float(sample), 100)
+    dd = round(float(degrees) + float(minutes) / 60, 6)
+    if head in {"S", "W"}:
+        dd *= -1
+    return dd
+
+
+def parser(line):
+    info = line.split(",")
+    if len(info) == 13:
+        doc = {
+            "device": info[0],
+            "operation": info[1],
+            "datetime": datetime.strptime(info[2], "%y%m%d%H%M%S").strftime("%Y-%m-%d %H:%M:%S"),
+            "unk1": info[3],
+            "F": info[4],
+            # "clock": datetime.strptime(info[5], "%H%M%S.%U").time(),
+            "A": info[6],
+            "location": {
+                "lat": dms2dd(info[7], info[8]),
+                "lon": dms2dd(info[9], info[10]),
+            },
+            # "lat": dms2dd(info[7]),
+            # "N": info[8],
+            # "lon": dms2dd(info[9]),
+            # "E": info[10],
+            "pitch": info[11],
+            "heading": info[12]
+        }
+        return doc, info[2]
+
+
+devices = set()
+
+
+# with open("locations.csv", "r") as file:
+#     for l in file:
+#         clean = l.split(",")
+#     last = int(clean[0]) + 1
+# print("waiting for location %d" % last)
+
+
+async def listener(reader, writer):
+    while True:
+        try:
+            data = await reader.readuntil(separator=b';')
+            address = writer.get_extra_info('peername')
+            print(f"From {address!r}, Received {data!r}")
+
+            message = data.decode()
+            if not message:
+                break
+
+            if message.startswith('##'):
+                devices.add(message.rstrip(";").split(",")[1].split(":")[-1])
+            elif message.startswith(tuple(devices)):
+                pass
+            elif message.startswith('imei:'):
+                document, identifier = parser(message.rstrip(";"))
+                r = es.index(index='cobra', document=document, id=identifier)
+            else:
+                print("what the fuck!")
+
+            writer.write(b'ON')
+            await writer.drain()
+
+        except asyncio.exceptions.IncompleteReadError:
+            break
+
+    print("Closing the connection")
+    writer.close()
+    await writer.wait_closed()
+    print("Connection closed")
+
+
+async def main():
+    server = await asyncio.start_server(listener, "0.0.0.0", 5000)
+
+    address = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+    print(f'Serving on {address}')
+
+    async with server:
+        await server.serve_forever()
+
+
+asyncio.run(main())
---
+++ serve_map_from_elastic.py
@@ -0,0 +1,79 @@
+# import pandas
+import folium
+import flask
+import sys
+# import os
+
+import elasticsearch
+from datetime import datetime, timedelta
+
+app = flask.Flask("mapping")
+
+es = elasticsearch.Elasticsearch(["elastic:9200"])
+if not es.indices.exists(index='cobra'):
+    sys.exit(1)
+
+query = {
+    "sort": [
+        {"datetime": {"order": "asc"}},  # , "format": "strict_date_optional_time_nanos"
+    ],
+    "query": {
+        "bool": {
+            "filter": [{
+                "range": {
+                    "datetime": {
+                        "gte": (datetime.now() - timedelta(days=2)).strftime("%Y-%m-%d %H:%M:%S"),
+                        "lte": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    }
+                }
+            }]
+        }
+    },
+    "size": 1000
+}
+
+
+def get_map():
+    tracks = [[]]
+    parked = True
+    res = es.search(index="cobra", body=query, )
+    hits = res["hits"]["hits"]
+    for hit in hits:
+        row = hit["_source"]
+        if row["operation"] == "acc on":
+            parked = False
+            tracks.append([(row["location"]["lat"], row["location"]["lon"])])
+        elif row["operation"] == "acc off":
+            parked = True
+            tracks[-1].append((row["location"]["lat"], row["location"]["lon"]))
+        elif not parked:
+            tracks[-1].append((row["location"]["lat"], row["location"]["lon"]))
+
+    tracks = tracks[1:]
+    my_map = folium.Map(location=tracks[-1][-1], tiles='OpenStreetMap', zoom_start=16)
+    for i, track in enumerate(tracks):
+        fg = folium.FeatureGroup(name="Track %d" % i, show=False)
+        folium.PolyLine(track, color="blue", weight=10, opacity=0.5).add_to(fg)
+        folium.Marker(location=track[0],
+                      icon=folium.Icon(color="green", icon="car", prefix='fa')).add_to(fg)
+        if not parked and i == len(tracks) - 1:
+            folium.Marker(location=track[-1],
+                          icon=folium.Icon(color="blue", icon="car", prefix='fa')).add_to(fg)
+        else:
+            folium.Marker(location=track[-1],
+                          icon=folium.Icon(color="red", icon="car", prefix='fa')).add_to(fg)
+        if i == len(tracks) - 1:
+            fg.show = True
+        fg.add_to(my_map)
+
+    folium.LayerControl().add_to(my_map)
+    return my_map
+
+
+@app.route('/')
+def index():
+    return get_map().get_root().render()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
